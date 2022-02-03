1b3242c7a728d26745398e4a4580fc772082b6a0
andregrandon <gran1360@mylaurier.ca>
2022-02-03 11:43:38 -0500

    favicon

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool( os.environ.get('DEBUG', False) )
+DEBUG = bool( os.environ.get('DEBUG', True) )
 
 ALLOWED_HOSTS =  ['starsocial-andregrandon.herokuapp.com','198.211.99.20', 'localhost', '127.0.0.1']
 
